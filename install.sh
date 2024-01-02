a558cff9b084d5cfcfe66e11d3afd1b8964c7872
Adam Taranto <adam.p.taranto@gmail.com>
2024-01-03 01:19:59 +1100

    do not build conda available packages

--- install.sh
+++ install.sh
@@ -1,43 +1,11 @@
+#!/bin/sh
+
 wd=$(pwd)
-chmod 764 */*/*/*
-chmod 764 */*/*
-chmod 764 */*
-chmod 764 *
+chmod 764 bin/*/*
+chmod 764 bin/*/*/*
 
-cd  idba
-./build.sh
-
-cd $wd
-cd metaspades/bin/
-p=$(pwd)
-ln -s "$p"/spades.py "$p"/metaspades.py
-
-cd "$wd"/trnascanSE
-tar -xvf infernal-1.1.3.tar.gz
-cd infernal-1.1.3
-./configure --prefix=$(pwd)/exec
-make
-make install
-
-cd "$wd"/trnascanSE
-tar -xvf automake-1.13.4.tar.gz
-cd automake-1.13.4
-./configure --prefix=$(pwd)/exec
-make
-make install
-
-cd "$wd"/trnascanSE
-tar -xvf trnascan-se-2.0.6.tar.gz
-cd tRNAscan-SE-2.0
-./configure --prefix=$(pwd)/exec
-cd "$wd"/trnascanSE
-eval "sed -i -e 's%missing\ automake\ 1\.13%missing $(pwd)\/automake\-1\.13\.4\/bin\/%g'" ./tRNAscan-SE-2.0/Makefile
-cd tRNAscan-SE-2.0
-make 
-make install
-
-cd "$wd"/trnascanSE
-ln -s "$wd"/trnascanSE/infernal-1.1.3/src/* "$wd"/trnascanSE/tRNAscan-SE-2.0/exec/bin/
+# Might need to fix refs to this
+#ln -s "$p"/spades.py "$p"/metaspades.py
 
 cd "$wd"/mitfi
 tar -xvf infernal-1.0.2.tar.gz
