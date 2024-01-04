43d45535fa4a887bb0c0b0c75e276cedf7ddcce4
Adam Taranto <adam.p.taranto@gmail.com>
2024-01-04 12:59:31 +1100

    add logging

--- src/mitofinder/utils.py
+++ src/mitofinder/utils.py
@@ -3,16 +3,17 @@ import sys
 import subprocess
 from collections import Counter
 import os.path
+import logging
 
 
 def is_avail(tool_names, kill=True):
     for tool_name in tool_names:
         tool_path = shutil.which(tool_name)
         if tool_path is not None:
-            print(f"{tool_name} is available on the PATH: {tool_path}")
+            logging.info(f"{tool_name} is available on the PATH: {tool_path}")
             return True
         else:
-            print(f"Error: {tool_name} not found on the PATH.")
+            logging.error(f"Error: {tool_name} not found on the PATH.")
             if kill:
                 sys.exit(1)
             else:
@@ -22,18 +23,18 @@ def is_avail(tool_names, kill=True):
 def check_files(file_names):
     for file_name in file_names:
         if file_name and os.path.exists(file_name):
-            print(f"File found: '{file_name}'")
+            logging.info(f"File found: '{file_name}'")
         elif file_name:
-            print(f"File does not exist: '{file_name}'")
+            logging.error(f"File does not exist: '{file_name}'")
             exit(1)
 
 
 def get_abs_if_found(file_name):
     if file_name and os.path.exists(file_name):
-        print(f"File found: '{file_name}'")
+        logging.info(f"File found: '{file_name}'")
         return os.path.abspath(file_name)
     elif file_name:
-        print(f"File does not exist: '{file_name}'")
+        logging.error(f"File does not exist: '{file_name}'")
         exit(1)
 
 
@@ -67,9 +68,17 @@ def setup_directory(directory_path):
     if not os.path.exists(directory_path):
         # If it doesn't exist, create the directory
         os.makedirs(directory_path)
-        print(f"Directory created: '{directory_path}'")
+        logging.info(f"Directory created: '{directory_path}'")
     else:
         # If it exists, clean existing files
         shutil.rmtree(directory_path)
         os.makedirs(directory_path)
-        print(f"Directory cleaned: '{directory_path}'")
+        logging.info(f"Directory cleaned: '{directory_path}'")
+
+
+def check_if_string_in_file(f, string):
+    with open(f, "r") as read_obj:
+        for line in read_obj:
+            if string in line:
+                return True
+    return False
